30f3a0153f05fcc1076621e24608905af69bff4d
iRave <till.busse@gmail.com>
2018-02-12 21:49:18 +0100

    YalpStore: Update to 0.35

--- YalpStore/Android.mk
+++ YalpStore/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := YalpStore
-LOCAL_SRC_FILES := com.github.yeriomin.yalpstore_29.apk
+LOCAL_SRC_FILES := com.github.yeriomin.yalpstore_35.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
